c0baa3b31f372ccfc0ac7658e5609d2b5ad54db5
Francisco Mancardi <francisco.mancardi@gmail.com>
2018-05-23 17:41:03 +0200

    added check to allow working with MantisBT Standard GUI (avoiding crasH :) )

--- core/JWTUtils.class.php
+++ core/JWTUtils.class.php
@@ -14,13 +14,19 @@ class JWTUtils {
     	$jwt = gpc_get_string('token',null);
 	  }	
 
-	  $uname = user_get_username(auth_get_current_user_id()); 
-	  $unameOnJWT = self::getLoginName($jwt);
-	  if( strcasecmp($uname, $unameOnJWT)  != 0 ){
-	    auth_logout();
-	    $t_user_id = user_get_id_by_name( $unameOnJWT );
-	    auth_login_user( $t_user_id );
-	  } 
+	  // $jwt == null 
+	  // non si sta accedendo via integrazione, va via la GUI
+	  // standard di MantisBT
+	  //
+	  if( null != $jwt ) {
+		  $uname = user_get_username(auth_get_current_user_id()); 
+		  $unameOnJWT = self::getLoginName($jwt);
+		  if( strcasecmp($uname, $unameOnJWT)  != 0 ){
+		    auth_logout();
+		    $t_user_id = user_get_id_by_name( $unameOnJWT );
+		    auth_login_user( $t_user_id );
+		  } 
+	  }
 	} 
 
 	/**
@@ -65,44 +71,39 @@ class JWTUtils {
   static function getCustomFields($jwt) {
       $cfSet = '';
 
-      //echo __METHOD__;
-      //die();
+      if( null !== $jwt ) {
+	      /*
+	      Useful for debug without signature verification
+	        https://github.com/firebase/php-jwt/issues/68
+	        */
+	      list($header, $payload, $signature) = explode (".", $jwt);
+	      print base64_decode($payload);
+	      $plain = json_decode(base64_decode($payload));
+	      
+	      if( property_exists($plain, 'additionalData') ) {
+	        $cfSet = $plain->additionalData; 
+	      }
+	        
+	        /*
+	      if($jwt != '') {
+	          $matahari = JWTSSOPlugin::getSecretKey();
+	          try {
+	            $plain = JWT::decode($jwt, $matahari,array(JWTSSOPlugin::ALGORITHM),
+	                                 null, array('typ' => JWTSSOPlugin::TYP));
+	          } catch (Exception $e) {
+	            echo __METHOD__ . ' :: Caught exception: ',  $e->getMessage(), "\n";
+	            echo '<pre>';
+	            var_dump($e);
+	            echo '</pre>';
+	            die();
+	          }
 
-      /*
-      Useful for debug without signature verification
-        https://github.com/firebase/php-jwt/issues/68
-        */
-      list($header, $payload, $signature) = explode (".", $jwt);
-      print base64_decode($payload);
-      $plain = json_decode(base64_decode($payload));
-      
-      //echo __FUNCTION__;
-      //var_dump($plain);
-      //die();
-      if( property_exists($plain, 'additionalData') ) {
-        $cfSet = $plain->additionalData; 
+	          if( property_exists($plain, 'additionalData') ) {
+	            $cfSet = $plain->additionalData; 
+	          }
+	      }
+	      */
       }
-        
-        /*
-      if($jwt != '') {
-          $matahari = JWTSSOPlugin::getSecretKey();
-          try {
-            $plain = JWT::decode($jwt, $matahari,array(JWTSSOPlugin::ALGORITHM),
-                                 null, array('typ' => JWTSSOPlugin::TYP));
-          } catch (Exception $e) {
-            echo __METHOD__ . ' :: Caught exception: ',  $e->getMessage(), "\n";
-            echo '<pre>';
-            var_dump($e);
-            echo '</pre>';
-            die();
-          }
-
-          if( property_exists($plain, 'additionalData') ) {
-            $cfSet = $plain->additionalData; 
-          }
-      }
-      */
-
       return $cfSet;
   }
 }
